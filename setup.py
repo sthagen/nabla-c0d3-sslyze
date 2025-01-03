b74dba2894b14dfa257c8a52ba360f8f8e411ad3
Alban Diquet <ad@datatheorem.io>
2025-01-03 21:41:24 +0100

    [#668]Enable Python 3.13 in CI

--- setup.py
+++ setup.py
@@ -76,6 +76,7 @@ setup(
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
         "Topic :: System :: Networking",
         "Topic :: System :: Monitoring",
         "Topic :: System :: Networking :: Monitoring",
